e3da36535fadfb6e2087efd4b57180b46e59d582
Vishal Kumar <vkumar971660@gmail.com>
2024-09-21 09:26:26 +0530

    Episode-7 end

--- src/app.js
+++ src/app.js
@@ -4,7 +4,78 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json());
 
+// API TO FIND ONE USER
+app.get("/user", async (req, res) => {
+    //const emailId = req.body.emailId;
+    //const user = await User.find({ emailId: emailId });
 
+    try {
+        const user = await User.find(req.body);
+        // console.log(user);
+        if (user.length === 0) {
+            res.send("User Not found");
+        }
+        else {
+            res.send(user);
+        }
+    }
+    catch (err) {
+        res.send("some error has been arised")
+    }
+
+})
+
+//API TO FIND ALL USER
+app.get("/feed", async (req, res) => {
+
+    try {
+        const user = await User.find({});
+        // console.log(user);
+        if (user.length === 0) {
+            res.send("User Not found");
+        }
+        else {
+            res.send(user);
+        }
+    }
+    catch (err) {
+        res.send("some error has been arised");
+    }
+
+
+})
+
+
+//API FOR DELETE A USER
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        await User.findByIdAndDelete({ _id: userId });
+        res.send("user Deleted Successfully");
+    }
+    catch (err) {
+        res.send("some error has been arised");
+    }
+})
+
+//API FOR UPDATE USER INFORMATION
+app.patch("/user", async (req, res) => {
+
+    const userId = req.body.userId;
+    const data = req.body;
+
+
+    try {
+        const beforeUpdate = await User.findByIdAndUpdate({ _id: userId }, data);
+
+        res.send(beforeUpdate);
+    }
+    catch (err) {
+        res.send("some error has been arised");
+    }
+})
+
+//API TO SIGNUP
 app.post("/signup", async (req, res) => {
     console.log(req.body);
     const user = new User(req.body);
